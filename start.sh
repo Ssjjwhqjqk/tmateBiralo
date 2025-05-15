a9f9f7131af9ecddf7276e08c2fb91f290789931
Birat <107595971+Ssjjwhqjqk@users.noreply.github.com>
2025-05-15 10:50:09 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,34 @@
 
 service ssh start
 
+# Start tmate session in detached mode with socket
 tmate -S /tmp/tmate.sock new-session -d
 
-# Wait for the tmate session to be ready
-while ! tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > /dev/null 2>&1; do
+echo "Waiting for tmate session..."
+
+# Wait up to 30 seconds for tmate SSH socket to be available
+for i in {1..30}; do
+  tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' && break
   sleep 1
 done
 
-# Write the SSH and web links to the file served by nginx
-echo "🧑‍💻 Your VPS is ready!" > /var/www/html/index.html
-echo "" >> /var/www/html/index.html
-echo "SSH: $(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')" >> /var/www/html/index.html
-echo "Web: $(tmate -S /tmp/tmate.sock display -p '#{tmate_web}')" >> /var/www/html/index.html
+echo "tmate session info:"
 
-# Start nginx in the foreground
+# Capture the info
+SSH_LINK=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
+WEB_LINK=$(tmate -S /tmp/tmate.sock display -p '#{tmate_web}')
+
+echo "SSH_LINK=$SSH_LINK"
+echo "WEB_LINK=$WEB_LINK"
+
+# Write HTML page with the links
+cat <<EOF > /var/www/html/index.html
+<h2>Your VPS is ready!</h2>
+<p>SSH connection:</p>
+<pre>$SSH_LINK</pre>
+<p>Web connection:</p>
+<pre>$WEB_LINK</pre>
+EOF
+
+# Start nginx
 nginx -g 'daemon off;'
