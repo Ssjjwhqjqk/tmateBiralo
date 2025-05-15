6014c4ce017e687456f09493290f795a51fd261f
Birat <107595971+Ssjjwhqjqk@users.noreply.github.com>
2025-05-15 10:35:47 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# Start SSH service
 service ssh start
 
-# Start tmate session in the background using a socket
 tmate -S /tmp/tmate.sock new-session -d
 
-# Wait for tmate session to be ready
+# Wait for the tmate session to be ready
 while ! tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > /dev/null 2>&1; do
   sleep 1
 done
 
-# Write connection info to web directory
+# Write the SSH and web links to the file served by nginx
 echo "🧑‍💻 Your VPS is ready!" > /var/www/html/index.html
 echo "" >> /var/www/html/index.html
-tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' >> /var/www/html/index.html
-tmate -S /tmp/tmate.sock display -p '#{tmate_web}' >> /var/www/html/index.html
+echo "SSH: $(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')" >> /var/www/html/index.html
+echo "Web: $(tmate -S /tmp/tmate.sock display -p '#{tmate_web}')" >> /var/www/html/index.html
 
-# Start nginx in foreground (to keep container alive)
+# Start nginx in the foreground
 nginx -g 'daemon off;'
